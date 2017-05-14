fdda2321ba7b2db6bca31b46ee4bfe6dd118a584
Andre Sutherland <andrelsf95@gmail.com>
2017-05-14 21:44:29 +0100

    Corrected field types for repo info sent by dockerhub.

--- webhook/types.go
+++ webhook/types.go
@@ -21,19 +21,19 @@ type DockerHubPushData struct {
 // DockerHubRepository provides that data structure for all information about the docker
 // repository the build occured for.
 type DockerHubRepository struct {
-	CommentCount    string  `json:"comment_count"`
-	DateCreated     float64 `json:"date_created"`
-	Description     string  `json:"description"`
-	Dockerfile      string  `json:"dockerfile"`
-	FullDescription string  `json:"full_description"`
-	IsOfficial      bool    `json:"is_official"`
-	IsPrivate       bool    `json:"is_private"`
-	IsTrusted       bool    `json:"is_trusted"`
-	Name            string  `json:"name"`
-	Namespace       string  `json:"namespace"`
-	Owner           string  `json:"owner"`
-	RepoName        string  `json:"repo_name"`
-	RepoURL         string  `json:"repo_url"`
-	StarCount       int64   `json:"star_count"`
-	Status          string  `json:"status"`
+	CommentCount    int64  `json:"comment_count"`
+	DateCreated     int64  `json:"date_created"`
+	Description     string `json:"description"`
+	Dockerfile      string `json:"dockerfile"`
+	FullDescription string `json:"full_description"`
+	IsOfficial      bool   `json:"is_official"`
+	IsPrivate       bool   `json:"is_private"`
+	IsTrusted       bool   `json:"is_trusted"`
+	Name            string `json:"name"`
+	Namespace       string `json:"namespace"`
+	Owner           string `json:"owner"`
+	RepoName        string `json:"repo_name"`
+	RepoURL         string `json:"repo_url"`
+	StarCount       int64  `json:"star_count"`
+	Status          string `json:"status"`
 }
